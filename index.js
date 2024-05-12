f19e071f8f6a0331fe4617a869526a5f7cf94e69
Yuriy Dobryanskiy <dobryansky@i.ua>
2024-05-13 01:37:11 +0300

    fix

--- index.js
+++ index.js
@@ -7,14 +7,13 @@ import {
 } from "./contacts.js";
 
 program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
+  .option("-a, --action <action>", "choose action")
+  .option("-i, --id <id>", "user id")
+  .option("-n, --name <name>", "user name")
+  .option("-e, --email <email>", "user email")
+  .option("-p, --phone <phone>", "user phone");
 
 program.parse();
-const options = program.opts();
 
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
@@ -40,9 +39,9 @@ async function invokeAction({ action, id, name, email, phone }) {
       console.warn("Unknown action type!");
   }
 }
-invokeAction(options);
+invokeAction(program.opts());
 
 // node index.js -a list
 // node index.js -a get -i 05olLMgyVQdWRwgKfg5J6
 // node index.js -a add -n Mango -e mango@gmail.com -p 322-22-22
-// node index.js -a remove -i 1DEXoP8AuCGYc1YgoQ6hw
+// node index.js -a remove -i 05olLMgyVQdWRwgKfg5J6
